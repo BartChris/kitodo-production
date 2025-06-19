2742b409fc46ed8b5e871d83a0e5b71a65cbc57e
BartChris <christoph.bartmann@gmx.de>
2025-06-19 22:54:03 +0200

    Add license headers

--- Kitodo-DataManagement/src/main/java/org/kitodo/data/database/dtos/TaskRowDTO.java
+++ Kitodo-DataManagement/src/main/java/org/kitodo/data/database/dtos/TaskRowDTO.java
@@ -1,7 +1,14 @@
 /*
  * (c) Kitodo. Key to digital objects e. V. <contact@kitodo.org>
- * Licensed under GNU GPL v3 or later – see GPL3-License.txt.
+ *
+ * This file is part of the Kitodo project.
+ *
+ * It is licensed under GNU General Public License version 3 or later.
+ *
+ * For the full copyright and license information, please read the
+ * GPL3-License.txt file that was distributed with this source code.
  */
+
 package org.kitodo.data.database.dtos;
 
 import java.io.Serializable;
